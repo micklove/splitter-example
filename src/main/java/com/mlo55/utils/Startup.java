c9340be7954b54537e952f02c376b40cd4dae11c
micklove <micklove+github@gmail.com>
2015-08-19 01:51:04 +1000

    add test files for playing with xslt

--- src/main/java/com/mlo55/utils/Startup.java
+++ src/main/java/com/mlo55/utils/Startup.java
@@ -12,7 +12,7 @@ public class Startup {
 	@SuppressWarnings({ "resource", "unused" })
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-				"/META-INF/spring/si-components.xml");
+				"/META-INF/spring/si-chained-components.xml");
 //		while (true) {
 //		}
 	}
